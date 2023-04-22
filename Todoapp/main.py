4d00aa4e4d5759db2d1b7414f0ee6e2ec3cf3d78
DineshReddy26 <dinesh.milliondesis@gmail.com>
2023-04-22 08:23:35 +0530

    update

--- Todoapp/main.py
+++ Todoapp/main.py
@@ -5,7 +5,6 @@ from sqlalchemy.orm import Session
 
 from fastapi import FastAPI, Depends, HTTPException, Path
 from starlette import status
-
 import models
 from models import Todos
 from db import engine, SessionLocal
@@ -47,6 +46,21 @@ async def read_todo(db: db_dependency,todo_id:int=Path(gt=0)):
 @app.post("/todo",status_code=status.HTTP_201_CREATED)
 async def create_table(db:db_dependency,todo_request:Todorequest):
     todo_model=Todos(**todo_request.dict())
+
+    db.add(todo_model)
+    db.commit()
+
+
+@app.put("/todo/{todo_id}",status_code=status.HTTP_204_NO_CONTENT)
+async def update_todo(db:db_dependency,todo_request:Todorequest,todo_id:int= Path(gt=0)):
+    todo_model=db.query(Todos).filter(Todos.id==todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=404, detail='Todo not found.')
+
+    todo_model.title = todo_request.title
+    todo_model.description=todo_request.description
+    todo_model.priority=todo_request.priority
+    todo_model.completed=todo_request.completed
 
     db.add(todo_model)
     db.commit()
