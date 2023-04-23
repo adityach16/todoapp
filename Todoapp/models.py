ea3ac7c8937efc92d1a90781543982a555744ff9
DineshReddy26 <dinesh.milliondesis@gmail.com>
2023-04-23 07:11:52 +0530

    post into user table

--- Todoapp/models.py
+++ Todoapp/models.py
@@ -6,7 +6,7 @@ class Users(Base):
 
     id=Column(Integer,primary_key=True,index=True)
     email=Column(String,unique=True)
-    username=Column(String,unique=True)
+    username=Column(String)
     first_name=Column(String)
     last_name = Column(String)
     hashed_password=Column(String)
@@ -21,4 +21,4 @@ class Todos(Base):
     description=Column(String)
     priority=Column(Integer)
     completed=Column(Boolean, default=False)
-    owner_id=Column(Integer,ForeignKey("users.id"))
+    owner_id=Column(Integer, ForeignKey("users.id"))
