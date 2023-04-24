353f4ae99af298a74285afde7686d6cc12286f84
DineshReddy26 <dinesh.milliondesis@gmail.com>
2023-04-24 06:11:17 +0530

    Authentication Enhancements

--- Todoapp/routers/auth.py
+++ Todoapp/routers/auth.py
@@ -10,13 +10,13 @@ from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordRequestForm,OAuth2PasswordBearer
 from jose import jwt, JWTError
 
-router = APIRouter()
+router = APIRouter(prefix='/auth',tags=['auth'])
 
 SECRET_KEY='as3fg4h2h5ih642nnb62h2j2g42bbn52hj62jhg26k2j62jh678943geh4'
 ALGORITHM='HS256'
 
 bcrypt_context=CryptContext(schemes=['bcrypt'], deprecated='auto')
-OAuth2_Bearer=OAuth2PasswordBearer(tokenUrl='token')
+OAuth2_Bearer=OAuth2PasswordBearer(tokenUrl='auth/token')
 
 
 class CreateUserRequest(BaseModel):
@@ -69,7 +69,7 @@ async def get_current_user(token:Annotated[str,Depends(OAuth2_Bearer)]):
     except JWTError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail='could not validate user')
 
-@router.post("/auth",status_code=status.HTTP_201_CREATED)
+@router.post("/",status_code=status.HTTP_201_CREATED)
 async def create_user(db:db_dependency,create_user_request: CreateUserRequest):
     create_user_model = Users(
         email=create_user_request.email,
@@ -89,7 +89,7 @@ async def create_user(db:db_dependency,create_user_request: CreateUserRequest):
 async def login_for_access_token(form_data:Annotated[OAuth2PasswordRequestForm,Depends()],db:db_dependency):
     user=authen_user(form_data.username,form_data.password,db)
     if not user:
-        return 'Failed authentication'
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail='could not validate user')
     token=create_access_token(user.username,user.id,timedelta(minutes=20))
 
     return {'access_token':token,'token_type':'bearer'}
